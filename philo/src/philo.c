5c0b3b7517babab725b891a4b07a7338e6f930ec
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-11-12 20:08:33 +0100

    removed excess functions

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,68 +6,13 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 19:01:40 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/12 19:46:42 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/12 20:08:00 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
-#include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-#include <stdio.h>
-//Function represents a thread that checks for philosophers starving.
-static void	*medical_examiner(void *arg)
-{
-	t_data	*data;
-	t_philo	*philo;
-	int		i;
-
-	data = (t_data *) arg;
-	philo = data->table->first_ph;
-	i = 1;
-	while (i > 0 && i <= data->table->members)
-	{
-		workoutts(data);
-		if (data->tmst->absms - philo->mealtime >= data->rules->timedie)
-		{
-			if (data->starved == 1)
-				printevent(data, philo, 'd');
-			philo->state->starved = 1;
-			data->starved += 1;
-		}
-		philo = philo->next_ph;
-		i++;
-	}
-	return (0);
-}
-
-//Function represents a philosopher thread.
-static void	*philosopher(void *arg)
-{
-	t_data	*data;
-	t_philo	*philo;
-
-	data = (t_data *) arg;
-	philo = findphilonum(data);
-	if (beginning(data) == 0)
-		return (0);
-	printevent(data, philo, 'p');
-	pthread_mutex_lock(&philo->prev_f->mfork);
-	printevent(data, philo, 'f');
-	pthread_mutex_lock(&philo->next_f->mfork);
-	printevent(data, philo, 'e');
-	usleep(1000 * data->rules->timeeat);
-	feeding(data, philo);
-	pthread_mutex_unlock(&philo->prev_f->mfork);
-	philo->prev_f->available = 1;
-	pthread_mutex_unlock(&philo->next_f->mfork);
-	philo->next_f->available = 1;
-	printevent(data, philo, 's');
-	usleep(1000 * data->rules->timeslp);
-	printevent(data, philo, 't');
-	return (0);
-}
 
 //Function initialises the table and philosopher threads.
 int	philosophers(t_set *rules)
@@ -90,7 +35,7 @@ int	philosophers(t_set *rules)
 		i++;
 	}
 	gettimeofday(&data->tmst->rt, 0);
-	if (threading(data) == -1)
+	if (threading(data, threads, i) == -1)
 		write(2, "Error.\n", 7);
 	freedata(data);
 	freetable(table);
@@ -138,4 +83,3 @@ int	main(int argc, char **argv)
 		write(2, "Error.\n", 7);
 	return (-1);
 }
-//*/
