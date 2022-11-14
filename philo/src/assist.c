967ea31ff48cb439d253cc098ff397f4755531eb
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-11-14 11:43:37 +0100

    made the examiner check constantly

--- philo/src/assist.c
+++ philo/src/assist.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 19:39:47 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/12 20:09:16 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/14 11:39:30 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,27 @@ void	*medical_examiner(void *arg)
 {
 	t_data	*data;
 	t_philo	*philo;
+	int		actionsstart;
 	int		i;
 
 	data = (t_data *) arg;
 	philo = data->table->first_ph;
-	i = 1;
-	while (i > 0 && i <= data->table->members)
+	actionsstart = data->actions;
+	while (data->actions <= actionsstart)
 	{
-		workoutts(data);
-		if (data->tmst->absms - philo->mealtime >= data->rules->timedie)
+		i = 1;
+		while (i > 0 && i <= data->table->members)
 		{
-			philo->state->starved = 1;
-			data->starved += 1;
-			if (data->starved == 1)
-				printevent(data, philo, 'd');
+			workoutts(data);
+			if (data->tmst->absms - philo->mealtime >= data->rules->timedie)
+			{
+				data->starved += 1;
+				if (data->starved == 1)
+					printevent(data, philo, 'd');
+			}
+			philo = philo->next_ph;
+			i++;
 		}
-		philo = philo->next_ph;
-		i++;
 	}
 	return (0);
 }
@@ -60,23 +64,28 @@ void	*philosopher(void *arg)
 	pthread_mutex_unlock(&philo->prev_f->mfork);
 	philo->prev_f->available = 1;
 	pthread_mutex_unlock(&philo->next_f->mfork);
-	philo->next_f->available = 1;
 	printevent(data, philo, 's');
+	philo->next_f->available = 1;
+	if ((data->eaten == data->table->members && data->rules->reqeat > 0))
+		return (0);
 	usleep(1000 * data->rules->timeslp);
 	printevent(data, philo, 't');
+	data->actions += 1;
 	return (0);
 }
 
 //Function performs actions at the beginning of each philosopher thread.
 int	beginning(t_data *data, t_philo *philo)
 {
-	usleep(100);
 	if ((philo->eatct >= data->rules->reqeat && data->rules->reqeat > 0) || \
-			philo->eatct > findmineat(data))
+			philo->eatct > findmineat(data) || \
+			data->eaten == data->table->members)
 		return (0);
-	if (philo->num % 2 == 0)
-		usleep(250);
+	if (philo->num % 2 == 1)
+		usleep(100);
 	if (philo->prev_f->available == 0 || philo->next_f->available == 0)
+		return (0);
+	if (data->starved > 0)
 		return (0);
 	return (1);
 }
@@ -89,14 +98,15 @@ int	threading(t_data *data, pthread_t *threads, int i)
 		if (pthread_create(&threads[0], 0, medical_examiner, data) != 0)
 			return (-1);
 		i = 1;
-		while (i <= data->rules->numphi)
+		while (i <= data->table->members)
 		{
 			data->philonum = i;
 			if (pthread_create(&threads[i], 0, philosopher, data) != 0)
 				return (-1);
 			i++;
 		}
-		while (i < data->rules->numphi)
+		i = 1;
+		while (i <= data->table->members)
 		{
 			if (pthread_join(threads[i], 0) != 0)
 				return (-1);
