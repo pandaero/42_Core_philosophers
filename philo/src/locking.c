e2527ada381d2897671632a738fc8f93dfcf5cc7
pandalaf <pandalaf@student.42wolfsburg.de>
2022-11-17 22:33:10 +0100

    substantial change to working of locking

--- philo/src/locking.c
+++ philo/src/locking.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 01:04:24 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/15 02:50:15 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/17 22:31:05 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,55 +14,55 @@
 #include <pthread.h>
 #include <unistd.h>
 
-static void	lockassist(t_data *data, t_philo *philo)
+static void	lockingforks_odd(t_data *data, t_philo *philo)
 {
 	pthread_mutex_lock(&philo->next_f->mfork);
-	philo->next_f->available = 0;
-	printevent(data, philo, 'p');
 	printevent(data, philo, 'f');
-	printevent(data, philo, 'e');
-	feeding(data, philo);
+	if (philo->prev_f)
+	{
+		pthread_mutex_lock(&philo->prev_f->mfork);
+		printevent(data, philo, 'p');
+		printevent(data, philo, 'e');
+		feeding(data, philo);
+	}
+	else
+	{
+		while (data->starved == 0)
+			usleep(100);
+	}
 }
 
 //Function handles locking forks.
 void	lockingforks(t_data *data, t_philo *philo)
 {
-	int	eatct;
-
-	eatct = philo->eatct;
-	while (data->starved == 0)
+	if (philo->num % 2 == 1)
+		lockingforks_odd(data, philo);
+	else
 	{
-		while (philo->prev_f->available == 0 && philo->next_f->available == 0)
-			usleep(500);
-		while (philo->prev_f->available == 1 || philo->next_f->available == 1)
+		pthread_mutex_lock(&philo->prev_f->mfork);
+		printevent(data, philo, 'p');
+		if (philo->next_f)
 		{
-			if (data->starved > 0)
-				return ;
-			pthread_mutex_lock(&philo->prev_f->mfork);
-			philo->prev_f->available = 0;
-			if (philo->next_f->available == 0)
-			{
-				usleep(100);
-				pthread_mutex_unlock(&philo->prev_f->mfork);
-				philo->prev_f->available = 1;
-				continue ;
-			}
-			lockassist(data, philo);
+			pthread_mutex_lock(&philo->next_f->mfork);
+			printevent(data, philo, 'f');
+			printevent(data, philo, 'e');
+			feeding(data, philo);
 		}
-		if (philo->eatct > eatct)
-			break ;
 	}
 }
 
 //Function handles unlocking forks.
-int	unlockingforks(t_data *data, t_philo *philo)
+void	unlockingforks(t_data *data, t_philo *philo)
 {
-	if (data->starved > 0)
-		return (0);
-	pthread_mutex_unlock(&philo->prev_f->mfork);
-	philo->prev_f->available = 1;
-	pthread_mutex_unlock(&philo->next_f->mfork);
-	philo->next_f->available = 1;
+	if (philo->num % 2 == 1)
+	{
+		pthread_mutex_unlock(&philo->next_f->mfork);
+		pthread_mutex_unlock(&philo->prev_f->mfork);
+	}
+	else
+	{
+		pthread_mutex_unlock(&philo->prev_f->mfork);
+		pthread_mutex_unlock(&philo->next_f->mfork);
+	}
 	printevent(data, philo, 's');
-	return (1);
 }
