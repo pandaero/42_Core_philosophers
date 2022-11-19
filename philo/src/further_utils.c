0dc600054dcb6e7ed0e769ec1618b8c18a1a58c3
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-11-19 16:26:30 +0100

    added custom sleep/wait function

--- philo/src/further_utils.c
+++ philo/src/further_utils.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 21:11:02 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/17 21:02:25 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/19 16:25:44 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,24 @@ int	randomnum(int max)
 	gettimeofday(&time, 0);
 	rand = (int) time.tv_usec % max;
 	return (rand);
+}
+
+//Function will wait for the input in ms.
+void	mswait(long wait)
+{
+	struct timeval	time;
+	struct timeval	starttime;
+	long			msstarttime;
+	long			msfulltime;
+
+	gettimeofday(&starttime, 0);
+	msstarttime = starttime.tv_sec * 1000 + starttime.tv_usec / 1000;
+	while (1)
+	{
+		gettimeofday(&time, 0);
+		msfulltime = time.tv_sec * 1000 + time.tv_usec / 1000;
+		if (msfulltime >= wait + msstarttime)
+			break ;
+		usleep(50);
+	}
 }
