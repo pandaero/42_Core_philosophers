3ee6ad8d6cb91a6f3c1a5b064ac30d33fd617d31
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-11-19 16:28:27 +0100

    added missing header

--- philo/src/further_utils.c
+++ philo/src/further_utils.c
@@ -6,12 +6,13 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 21:11:02 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/19 16:25:44 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/19 16:27:59 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 #include <sys/time.h>
+#include <unistd.h>
 
 //Function returns a random (up to) 5-digit number based on the current time.
 int	randomnum(int max)
